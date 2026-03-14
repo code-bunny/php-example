692370b361fc5684b6d45b843e299f7eeea7f964
David Roy <david.roy@codebunnies.com>
2026-03-14 22:55:35 +0000

    places cats

--- pages/home.php
+++ pages/home.php
@@ -18,24 +18,24 @@ $title = 'Home';
     <div class="flex flex-wrap md:-m-2 -m-1">
       <div class="flex flex-wrap w-1/2">
         <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://placecats.com/500/300">
         </div>
         <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://placecats.com/501/301">
         </div>
         <div class="md:p-2 p-1 w-full">
-          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360">
+          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://placecats.com/600/360">
         </div>
       </div>
       <div class="flex flex-wrap w-1/2">
         <div class="md:p-2 p-1 w-full">
-          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361">
+          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://placecats.com/601/361">
         </div>
         <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://placecats.com/502/302">
         </div>
         <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://placecats.com/503/303">
         </div>
       </div>
     </div>
