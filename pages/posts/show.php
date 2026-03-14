3a77cf9909547b35a6897a2e1eb8a06034236fe4
David Roy <david.roy@codebunnies.com>
2026-03-14 21:17:43 +0000

    uses tailblocks to style

--- pages/posts/show.php
+++ pages/posts/show.php
@@ -17,8 +17,8 @@ $title = $post->title;
 ?>
 
 <section class="text-gray-600 body-font overflow-hidden">
-    <div class="flex flex-wrap -m-12">
-        <div class="p-12 flex flex-col items-start">
+    <div class="px-5 py-12">
+        <div class="flex flex-col items-start">
             <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-mono tracking-widest">Posts</span>
             <h1 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4"><?= htmlspecialchars($post->title) ?></h1>
             <p class="leading-relaxed mb-8"><?= htmlspecialchars($post->body) ?></p>
