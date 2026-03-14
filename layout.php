8c6f23e9d0d8944af0199e104be7aaf3c50d3b76
David Roy <david.roy@codebunnies.com>
2026-03-14 21:52:38 +0000

    fixes header

--- layout.php
+++ layout.php
@@ -21,9 +21,8 @@
             <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
                 <a href="/" class="mr-5 hover:text-gray-900">Home</a>
                 <a href="/about" class="mr-5 hover:text-gray-900">About</a>
-                <a href="/contact" class="mr-5 hover:text-gray-900">Contact</a>
             </nav>
-            <a href="/posts/new" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">New Post
+            <a href="/contact" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Contact
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
