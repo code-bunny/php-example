107f9026c34138fa4e9c7f37ca30422ed1b214e3
David Roy <david.roy@codebunnies.com>
2026-03-14 23:17:08 +0000

    adds social links config

---
+++ config/social.php
@@ -0,0 +1,12 @@
+<?php
+
+// Social and repo links shown in the footer.
+// Set url to '' to hide that icon.
+
+return [
+    ['label' => 'GitHub',    'url' => 'https://github.com/code-bunny', 'icon' => 'github'],
+    ['label' => 'Facebook',  'url' => '', 'icon' => 'facebook'],
+    ['label' => 'Twitter',   'url' => '', 'icon' => 'twitter'],
+    ['label' => 'Instagram', 'url' => '', 'icon' => 'instagram'],
+    ['label' => 'LinkedIn',  'url' => '', 'icon' => 'linkedin'],
+];
